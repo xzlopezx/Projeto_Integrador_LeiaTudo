3156a091a66c01fea369c47ba95d5779f0ef5ad3
Gustavo Henrique de Araujo Alves <61398911+Hagnarok@users.noreply.github.com>
2025-09-24 00:24:35 +0000

    0.0.1

---
+++ public/includes/page-carousel.php
@@ -0,0 +1,24 @@
+<div id="carouselControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="public/css/imgs/hotdog-h500.png" alt="Hot Dog" class="d-block mx-auto img-fluid">
+        </div>
+        <div class="carousel-item">
+            <img src="public/css/imgs/hamburger-h500.png" alt="Hamburger" class="d-block mx-auto img-fluid">
+        </div>
+        <div class="carousel-item">
+            <img src="public/css/imgs/potato-h500.png" alt="Batata" class="d-block mx-auto img-fluid">
+        </div>
+        <div class="carousel-item">
+            <img src="public/css/imgs/snacks-h500.png" alt="Snacks" class="d-block mx-auto img-fluid">
+        </div>
+    </div>
+
+        
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </button>
+</div>
