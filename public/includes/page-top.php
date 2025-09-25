69b686c7d9d20ce766b166a2a743cc493a980e8a
Gustavo Henrique de Araujo Alves <61398911+Hagnarok@users.noreply.github.com>
2025-09-25 22:56:07 +0000

    0.0.2

--- public/includes/page-top.php
+++ public/includes/page-top.php
@@ -12,7 +12,7 @@
             <div class="collapse navbar-collapse" id="navbarMain">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Home Page</a>
+                        <a class="nav-link" href="#">Cadastrar E-Book</a>
                     </li>
 
                     <li class="nav-item dropdown">
