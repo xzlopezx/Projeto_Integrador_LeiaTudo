dca65d4e06886aebbf3f55b0f05fb099b6c7fd21
lopez <91160738+xzlopezx@users.noreply.github.com>
2025-09-26 00:58:16 +0000

    0.0.7

--- registro/main.php
+++ registro/main.php
@@ -16,7 +16,7 @@
             </div>
             <div class="mb-3">
                 <label for="senha" class="form-label">Senha</label>
-                <input type="password" name="senha" id="senha" class="form-control" requird>
+                <input type="password" name="senha" id="senha" class="form-control" required>
             </div>
             <div class="mb-3">
                 <label for="senha" class="form-label">Confirmar Senha</label>
