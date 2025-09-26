448445218b99554b32259844020a7692b3e5f7fe
lopez <91160738+xzlopezx@users.noreply.github.com>
2025-09-26 00:54:03 +0000

    0.0.6

---
+++ registro/main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Registro - LeiaTudo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="home-login">
+    <div class="container mt-5">
+        <h2>Registro</h2>
+        <!-- redireciona para main.php -->
+        <form action="/home/main.php" method="GET">
+            <div class="mb-3">
+                <label for="usuario" class="form-label">Usuário</label>
+                <input type="text" name="usuario" id="usuario" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="senha" class="form-label">Senha</label>
+                <input type="password" name="senha" id="senha" class="form-control" required>
+            </div>
+            <div class="mb-3">
+                <label for="senha" class="form-label">Confirmar Senha</label>
+                <input type="password" name="senha" id="senha" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Registrar-se</button>
+        </form>
+    </div>
+</body>
+</html>
